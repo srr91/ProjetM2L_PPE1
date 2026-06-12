f91eebba957f14fe59acab4d81a8609b2b7efb08
srr91 <enzoteixeira.pro91@gmail.com>
2026-06-12 15:23:21 +0200

    Delete SportConnect directory

--- SportConnect/app/Views/layout/pied-de-page.php
+++
@@ -1,6 +0,0 @@
-</main>
-<footer style="background:#111; color:#fff; text-align:center; padding:2rem 1rem;">
-    <p>© <?= date('Y') ?> SportConnect - Tous droits réservés.</p>
-</footer>
-</body>
-</html>
--- SportConnect/old/authentification/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../app/bootstrap.php';
-\App\Core\Auth::startSession();
-
-$authController = new \App\Controllers\AuthController(BASE_PATH);
-$authController->logout();
-?>
