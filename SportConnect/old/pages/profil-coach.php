3be1fbe4bd445f2c8596b135a3124b960f3c9d83
srr91 <enzoteixeira.pro91@gmail.com>
2026-06-12 14:21:21 +0200

    Delete SportConnect directory

--- SportConnect/old/pages/profil-coach.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Compat MVC : cette page devient une route.
-$coach_id = $_GET['id'] ?? 0;
-header('Location: /SportConnect/index.php?route=coach/show&id=' . urlencode((string)$coach_id));
-exit();
-?>
